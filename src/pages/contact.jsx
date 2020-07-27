f35cc704b0373c3415418815dda4a63fe079d68a
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-07-27 16:59:34 +0200

    Add maps and phone number sections to contact page.

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -3,11 +3,79 @@ import styled from 'styled-components'
 import { PageWrapper } from '~/utils/styles'
 import ContactFormContainer from '~/components/ContactFormContainer'
 
+const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 10px;
+  width: 90%;
+  padding: 20px;
+  /* border-bottom: 1px solid ${props => props.theme.colors.dark}; */
+  h2 {
+    font-size: ${props => props.theme.fontSize.medium};
+    margin-bottom: 20px;
+    padding-bottom: 5px;
+    border-bottom: 1px solid ${props => props.theme.colors.dark};
+  }
+  iframe {
+    margin: 10px 0 20px 0;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.5);
+  }
+
+  .phone {
+    color: ${props => props.theme.colors.darkLight};
+    &:hover {
+      color: ${props => props.theme.colors.pb};
+    }
+  }
+  
+  .map-1 {
+    margin-bottom: 40px;
+  }
+`
+
 function Contact() {
   return (
     <PageWrapper>
       <h1>Contact</h1>
-      <ContactFormContainer />
+      <Section>
+        <h2>Phone</h2>
+        <p>
+          Call or text us at{' '}
+          <a className="phone" href="tel:2148105668">
+            214-810-5668
+          </a>
+        </p>
+      </Section>
+      <Section>
+        <h2>Visit</h2>
+        <p>The Market at Luscombe Farms</p>
+        <iframe
+          className="map-1"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3333.8924540731714!2d-96.57324068454211!3d33.321626980811004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864c72709decad9f%3A0xb9cc805425d70991!2sLuscombe%20Farm%20Specialty%20Food%20Store!5e0!3m2!1sen!2sbe!4v1595860680095!5m2!1sen!2sbe"
+          width="400"
+          height="300"
+          frameBorder="0"
+          allowFullScreen
+          aria-hidden="false"
+          tabIndex="0"
+        ></iframe>
+        <p>Denison Farmers Market</p>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3317.180844244569!2d-96.54764442009964!3d33.755992380782075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864c85abd2206fe1%3A0x3d961609e51bad11!2sDowntown%20Denison%20Farmers%20Market!5e0!3m2!1sen!2sbe!4v1595860721838!5m2!1sen!2sbe"
+          width="400"
+          height="300"
+          frameBorder="0"
+          allowFullScreen
+          aria-hidden="false"
+          tabIndex="0"
+        ></iframe>
+      </Section>
+      <Section>
+        <h2>Send a message</h2>
+        <ContactFormContainer />
+      </Section>
     </PageWrapper>
   )
 }
